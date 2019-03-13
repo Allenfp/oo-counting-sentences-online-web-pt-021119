db2ea2bcbcd794798d1846d8e4d4c4406e79980c
Allen F Partlow <allenfp@gmail.com>
2019-03-13 06:10:34 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    temp = self.split(%r{[?.!]}) #
+    temp2 = temp.reject { |e| e.to_s.empty? }
+    temp2.length
   end
 end
